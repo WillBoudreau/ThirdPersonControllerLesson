e2f0cb1703daf07b131f847c9cb8073ea9ae520f
WillBoudreau <144450585+WillBoudreau@users.noreply.github.com>
2024-09-18 15:08:21 -0300

    Commented

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 // Sam Robichaud 
 // NSCC Truro 2024
@@ -18,7 +19,15 @@ public class InputManager : MonoBehaviour
     public float horizontalInput;
     public bool jumpInput;
     public Vector2 movementInput;
+    public Vector3 moveDirection = Vector3.zero;
     public float moveAmount;
+
+    //New Input
+    //Refernce to Script created by Unity
+    public PlayerInputActions playerMovement;
+    //Input Action Component
+    private InputAction move;
+    private InputAction fire;
 
     [Header("Camera Inputs")]
     public float scrollInput; // Scroll input for camera zoom
@@ -26,35 +35,44 @@ public class InputManager : MonoBehaviour
 
     public bool isPauseKeyPressed = false;
 
-
+    private void Awake()
+    {
+        playerMovement = new PlayerInputActions();
+        //Enables the new input
+        OnEnable();
+    }
     public void HandleAllInputs()
     {
-        HandleMovementInput();
-        HandleSprintingInput();
-        HandleJumpInput();
-        HandleCameraInput();
-        HandlePauseKeyInput();
+
+        //HandleMovementInput();
+        //HandleSprintingInput();
+        //HandleJumpInput();
+        //HandleCameraInput();
+        //HandlePauseKeyInput();
     }
 
     private void HandleCameraInput()
-    {        
-            // Get mouse input for the camera
-            cameraInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    {
+        // Get mouse input for the camera
+        cameraInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-            // Get scroll input for camera zoom
-            scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        // Get scroll input for camera zoom
+        scrollInput = Input.GetAxis("Mouse ScrollWheel");
 
-            // Send inputs to CameraManager
-            cameraManager.zoomInput = scrollInput;
-            cameraManager.cameraInput = cameraInput;        
+        // Send inputs to CameraManager
+        cameraManager.zoomInput = scrollInput;
+        cameraManager.cameraInput = cameraInput;
     }
 
     private void HandleMovementInput()
     {
-        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        horizontalInput = movementInput.x;
-        verticalInput = movementInput.y;
-        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        //moveDirection = playerMovement.ReadValue<Vector3>();
+
+        //movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //horizontalInput = movementInput.x;
+        //verticalInput = movementInput.y;
+        //moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+
     }
 
     private void HandlePauseKeyInput()
@@ -82,9 +100,27 @@ public class InputManager : MonoBehaviour
             playerLocomotionHandler.HandleJump(); // Trigger jump in locomotion handler
         }
     }
-
-
+    private void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire");
+    }
+    //Enables the new Input
+    private void OnEnable()
+    {
+        move = playerMovement.Player.Move;
+        move.Enable();
+        fire = playerMovement.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+    //Disables the new Input
+    private void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
+    }
 
 
 
 }
+
